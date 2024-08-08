b42649feb6341343432da8f17ed6e58ef59569e5
Tayfun Uenver <tayfun.uenver@telekom.de>
2024-08-08 21:19:03 +0200

    add create table instruction for events, positions, employees, activities, rel_employee_activities and library managers tables

--- init.sql
+++ init.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS libraries (
   street VARCHAR(100) NOT NULL,
   house_number VARCHAR(10) NOT NULL,
   city VARCHAR(50) NOT NULL,
-  postal_code VARCHAR(15) NOT NULL,
+  postal_code VARCHAR(10) NOT NULL,
   latitude DECIMAL(9.6) NOT NULL,
   longitude DECIMAL(9.6) NOT NULL,
   country_code VARCHAR(5) NOT NULL,
@@ -38,7 +38,8 @@ CREATE TABLE IF NOT EXISTS special_opening_hours (
 );
 
 CREATE TABLE IF NOT EXISTS events (
-  event_id SERIAL PRIMARY KEY fk_library_id INT NOT NULL,
+  event_id SERIAL PRIMARY KEY,
+  fk_library_id INT NOT NULL,
   floor SMALLINT,
   room VARCHAR(10),
   title VARCHAR(100) NOT NULL,
@@ -46,5 +47,54 @@ CREATE TABLE IF NOT EXISTS events (
   start_time TIMESTAMP NOT NULL,
   end_time TIMESTAMP NOT NULL,
   max_participants SMALLINT NOT NULL,
-  registered_participants SMALLINT FOREIGN KEY (fk_library_id) REFERENCES libraries (library_id) ON UPDATE CASCADE ON DELETE CASCADE
-)
+  registered_participants SMALLINT,
+  FOREIGN KEY (fk_library_id) REFERENCES libraries (library_id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS positions (
+  position_id SERIAL PRIMARY KEY,
+  position_name VARCHAR(100),
+  position_description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS employees (
+  employee_id SERIAL PRIMARY KEY,
+  fk_library_id INT NOT NULL,
+  fk_position_id INT NOT NULL,
+  fk_supervisor_id INT,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  birthdate DATE NOT NULL,
+  email VARCHAR(100) NOT NULL UNIQUE,
+  country_code VARCHAR(5),
+  landline_area_code VARCHAR(10),
+  landline_number VARCHAR(20),
+  hire_date DATE NOT NULL,
+  position VARCHAR(100) NOT NULL,
+  is_supervisor BOOLEAN DEFAULT FALSE NOT NULL,
+  FOREIGN KEY (fk_library_id) REFERENCES libraries (library_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+  FOREIGN KEY (fk_position_id) REFERENCES positions (position_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+  FOREIGN KEY (fk_supervisor_id) REFERENCES employees (employee_id) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE IF NOT EXISTS activities (
+  activity_id SERIAL PRIMARY KEY,
+  activity_name VARCHAR(100),
+  activity_description TEXT
+);
+
+CREATE TABLE IF NOT EXISTS rel_employee_activites (
+  fk_employee_id INT NOT NULL,
+  fk_activity_id INT NOT NULL,
+  FOREIGN KEY (fk_employee_id) REFERENCES employees (employee_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  FOREIGN KEY (fk_activity_id) REFERENCES activities (activity_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  PRIMARY KEY (fk_employee_id, fk_activity_id)
+);
+
+CREATE TABLE IF NOT EXISTS library_managers (
+  fk_library_id INT NOT NULL UNIQUE,
+  fk_employee_id INT NOT NULL UNIQUE,
+  FOREIGN KEY (fk_library_id) REFERENCES libraries (library_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+  FOREIGN KEY (fk_employee_id) REFERENCES employees (employee_id) ON UPDATE CASCADE ON DELETE RESTRICT,
+  PRIMARY KEY (fk_library_id, fk_employee_id)
+);
