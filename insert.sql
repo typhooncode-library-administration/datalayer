2099e948bda10fa571c69b01f16ee81a618fcb89
Tayfun Uenver <tayfunuenver@hotmail.de>
2024-08-16 18:59:51 +0200

    add: insert.sql file to with some datasets for testing

---
+++ insert.sql
@@ -0,0 +1,18 @@
+INSERT INTO
+    universities (university_name, postal_code)
+VALUES
+    ('Heinrich Heine University Düsseldorf', '40225'),
+    ('Düsseldorf University of Applied Sciences', '40476'),
+    ('University of Wuppertal', '42119'),
+    ('University of Duisburg-Essen - Campus Duisburg', '47057'),
+    ('Folkwang University of the Arts - Campus Essen', '45127'),
+    ('Bergische Universität Wuppertal - Campus Haspel', '42285'),
+    ('Hochschule Niederrhein - Campus Krefeld', '47798'),
+    ('University of Applied Sciences Ruhr West - Mülheim', '45479'),
+    ('University of Cologne', '50923'),
+    ('German Sport University Cologne', '50933'),
+    ('Cologne University of Applied Sciences', '50679'),
+    ('Hochschule Bonn-Rhein-Sieg', '53757'),
+    ('Rheinische Fachhochschule Köln', '50667'),
+    ('Hochschule Düsseldorf - Campus Derendorf', '40476'),
+    ('FOM University of Applied Sciences Düsseldorf', '40221');
